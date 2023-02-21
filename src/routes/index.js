78742896147b989439d3f271a4d267809bd750b3
haaeee <chungjm0711@gmail.com>
2023-02-22 00:30:13 +0900

    feat: 기본적인 라우트 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
+import member from '@/store/member'
+import HomeLayout from '@/views/layout/HomeLayout.vue'
+
+const authCheck = async function (to, from, next) {
+  try {
+    if (!member.getters.isLogin) {
+      this.$store.dispatch('member/REISSUE_ACCESS_TOKEN')
+    }
+  } catch (error) {
+    location.href = import.meta.env.VITE_HIWORKS_LOGIN_PAGE
+  }
+  next()
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,21 +19,27 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: HomeLayout,
+      children: [
+        {
+          path: '/',
+          // beforeEnter: authCheck,
+          name: 'home',
+          component: () => import('@/views/HomeView.vue'),
+        },
+        {
+          path: '/',
+          // beforeEnter: authCheck,
+          name: 'register',
+          component: () => import('@/views/HomeView.vue'),
+        },
+      ],
     },
     {
       path: '/login/oauth2/code/hiworks',
-      component: LoginView
-    }
-  ]
+      component: () => import('@/views/layout/AuthSuccess.vue'),
+    },
+  ],
 })
 
 export default router
