79ea3718e3c170d67e559c41e1802c24434b62ab
haaeee <chungjm0711@gmail.com>
2023-02-21 23:32:45 +0900

    feat: dateFormat filter 추가

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from '@/routes'
 import store from '@/store'
 import mdiVue from 'mdi-vue/v3'
 import * as mdiJs from '@mdi/js'
+import { formatDate } from '@/utils/filters'
 
 const app = createApp(App)
+
+app.config.globalProperties.$filters = {
+  formatDate,
+}
 
 app.use(router).use(store).use(mdiVue, {
   icons: mdiJs,
