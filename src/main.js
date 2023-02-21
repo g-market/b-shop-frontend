7d0b7375599c06c329c3a340d3cc3bc61e5c5821
haaeee <chungjm0711@gmail.com>
2023-02-21 23:31:54 +0900

    feat: mdiIcon 추가

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routes'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/routes'
+import store from '@/store'
+import mdiVue from 'mdi-vue/v3'
+import * as mdiJs from '@mdi/js'
 
 const app = createApp(App)
 
-app.use(router).use(store)
+app.use(router).use(store).use(mdiVue, {
+  icons: mdiJs,
+})
 
 app.mount('#app')
