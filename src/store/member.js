8f3692e4c32325a507aef81fee8dc0c423696136
정종민 <jmchung@gabia.com>
2023-02-22 14:54:47 +0900

    fix: vuex member 모듈화 추가

--- src/store/member.js
+++ src/store/member.js
@@ -1,5 +1,5 @@
-import { login, logout, reissueAccessToken } from '@/api/auth'
-import { fetchMember } from '@/api/member'
+import { login, logout, reissueAccessToken } from '@/api/authApi'
+import { fetchMember, patchMember } from '@/api/memberApi'
 
 export default {
   namespaced: true,
@@ -12,7 +12,6 @@ export default {
       role: '',
       grade: '',
     },
-    registerCompleted: false,
     token: '',
   },
   getters: {
@@ -23,19 +22,17 @@ export default {
   mutations: {
     login(state, data) {
       state.token = data.token
-      state.registerCompleted = data.registerCompleted
+      state.member.id = data.memberResponse.id
       state.member.email = data.memberResponse.email
-      // state.phoneNumber = memberResponse.phoneNumber
+      state.member.phoneNumber =
+        data.memberResponse.phoneNumber == null ? '' : state.member.phoneNumber
       state.member.name = data.memberResponse.name
-      // state.role = memberResponse.role
-      // state.grade = memberResponse.grade
+      state.member.role = data.memberResponse.role
+      state.member.grade = data.memberResponse.grade
     },
     logout(state) {
-      state.isLogin = false
       state.token = ''
-    },
-    setRegisterCompleted(state, registerCompleted) {
-      this.registerCompleted = registerCompleted
+      state.member = _initMember()
     },
     setMember(state, data) {
       state.member.id = data.id
@@ -46,13 +43,15 @@ export default {
     },
     setToken(state, data) {
       state.token = data.token
-      // state.expiredTime = data.expiredTime
+    },
+    setPhoneNumber(state, phoneNumber) {
+      console.log(phoneNumber)
+      state.member.phoneNumber = phoneNumber
     },
   },
   actions: {
     async LOGIN({ commit }, authCode) {
       const { data } = await login(authCode)
-      console.log(data)
       commit('login', data)
       return data
     },
@@ -60,15 +59,40 @@ export default {
       await logout()
       commit('logout')
     },
+    async REISSUE_ACCESS_TOKEN({ commit }) {
+      try {
+        const { data } = await reissueAccessToken()
+        commit('setToken', data.token)
+      } catch (error) {
+        console.log(error.response.data.message)
+      }
+    },
     async FETCH_MEMBER({ commit }) {
       const { data } = await fetchMember()
       console.log(data)
       commit('setMember', data)
       return data
     },
-    async REISSUE_ACCESS_TOKEN({ commit }) {
-      const { data } = await reissueAccessToken()
-      commit('setToken', data)
+    async UPDATE_MEMBER({ commit }, memberUpdateRequest) {
+      try {
+        const { data } = await patchMember(memberUpdateRequest)
+        console.log(memberUpdateRequest)
+        commit('setPhoneNumber', memberUpdateRequest.phoneNumber)
+        return data
+      } catch (error) {
+        console.log(error)
+      }
     },
   },
+}
+
+function _initMember() {
+  return {
+    id: 0,
+    email: '',
+    phoneNumber: '',
+    name: '',
+    role: '',
+    grade: '',
+  }
 }
