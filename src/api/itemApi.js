9a94a0905380ae7c9b5a9ba31f51d22d96d3faab
jaime <jmchung@gabia.com>
2023-02-27 00:46:42 +0900

    feat: Item 조회 API 추가

---
+++ src/api/itemApi.js
@@ -0,0 +1,18 @@
+import { instanceWithAuth as items } from '@/api/index'
+import store from '@/store'
+
+function fetchItems(year, page) {
+  const size = store.state.searchStatus.size
+  return items.get(`/items?year=${year}&page=${page}&size=${size}`)
+}
+
+function fetchItem(itemId) {
+  return items.get(`/items/${itemId}`)
+}
+
+// SearchComponent Year Category
+function fetchYears() {
+  return items.get('/years')
+}
+
+export { fetchItems, fetchItem, fetchYears }
