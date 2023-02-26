49fd694a1b9d6a222a5a56ee5491d77e9356eae1
jaime <jmchung@gabia.com>
2023-02-27 00:37:13 +0900

    feat: year이 변경되면 page는 첫 번째 페이지로 고정

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -48,7 +48,10 @@ export default {
     async changeSelectedYear(selectedYear) {
       this.$store.commit('searchStatus/setSelectedYear', selectedYear)
       const year = this.$store.state.searchStatus.selectedYear
-      await this.$store.dispatch('item/FETCH_ITEMS', year)
+      await this.$store.dispatch('item/FETCH_ITEMS', {
+        year,
+        page: 0,
+      })
     },
   },
 }
