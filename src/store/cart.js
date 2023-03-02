da97f0ef23b0102fd1f7fc91ca36d0c5e4bc67dd
jaime <jmchung@gabia.com>
2023-03-02 09:57:29 +0900

    feat: cart Module 추가

---
+++ src/store/cart.js
@@ -0,0 +1,28 @@
+import { createCart, deleteCart, fetchCarts } from '@/api/cartApi'
+
+export default {
+  namespaced: true,
+  state: {
+    carts: [],
+  },
+  getters: {},
+  mutations: {
+    fetchCarts(state, carts) {
+      state.carts = carts
+    },
+  },
+  actions: {
+    async FETCH_CARTS({ commit }) {
+      const { data } = await fetchCarts()
+      commit('fetchCarts', data)
+      return data
+    },
+    async CREATE_CART({ commit }, cartCreateRequest) {
+      const { data } = await createCart(cartCreateRequest)
+      return data
+    },
+    async DELETE_CART({ commit }, cartDeleteRequest) {
+      await deleteCart(cartDeleteRequest)
+    },
+  },
+}
