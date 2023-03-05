b6fea6cfa6a2ff2062010a54db98aba482b39ba1
jaime <jmchung@gabia.com>
2023-03-05 23:44:33 +0900

    feat: order Store vuex 주문 조회 로직 추가

--- src/store/order.js
+++ src/store/order.js
@@ -1,25 +1,39 @@
-import { createOrder } from '@/api/orderApi'
+import { createOrder, fetchOrder, fetchOrders } from '@/api/orderApi'
 
 export default {
   namespaced: true,
   state: {
     selectedOptions: [],
-    orderResponse: {},
+    orderInfoResponse: {},
+    orders: {},
   },
   getters: {},
   mutations: {
     setSelectedOptions(state, selectedOptions) {
       state.selectedOptions = selectedOptions
     },
-    setOrderResponse(state, orderCreateResponse) {
-      state.orderResponse = orderCreateResponse
-      console.log(orderCreateResponse)
+    setOrderInfoResponse(state, orderInfoResponse) {
+      state.orderInfoResponse = orderInfoResponse
+    },
+    setOrders(state, orders) {
+      state.orders = orders
     },
   },
   actions: {
     async CREATE_ORDER({ commit }, orderItemList) {
       const { data } = await createOrder(orderItemList)
-      commit('setOrderResponse', data)
+      return data
+    },
+    async FETCH_ORDER({ commit }, orderId) {
+      const { data } = await fetchOrder(orderId)
+      commit('setOrderInfoResponse', data)
+      return data
+    },
+    async FETCH_ORDERS({ commit }, dateParam) {
+      const { data } = await fetchOrders(dateParam)
+      console.log(data)
+      commit('setOrders', data)
+      return data
     },
   },
 }
