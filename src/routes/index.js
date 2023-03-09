7f9f663e6ee9480b3dfa231736152301262efb98
정종민 <jmchung@gabia.com>
2023-03-09 11:35:22 +0900

    remove: 테스트 용도 화면 제거

--- src/routes/index.js
+++ src/routes/index.js
@@ -61,11 +61,6 @@ const router = createRouter({
       path: '/:pathMatch(.*)*',
       component: () => import('@/views/NotFoundView.vue'),
     },
-    {
-      path: '/home',
-      name: 'test',
-      component: () => import('@/views/HomeView.vue'),
-    },
   ],
 })
 
