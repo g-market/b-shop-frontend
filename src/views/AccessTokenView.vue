e2f540fc3e900c32ea9c5a8312dcc15fbe4423c0
정종민 <jmchung@gabia.com>
2023-03-09 17:54:24 +0900

    feat: accessToken을 확인할 수 있는 view 추가

---
+++ src/views/AccessTokenView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <div class="card">
+      {{ token }}
+    </div>
+  </main>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState('member', ['token']),
+  },
+}
+</script>
