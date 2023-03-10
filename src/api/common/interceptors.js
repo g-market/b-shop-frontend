70d8a55f7162fccf33897e75061a7fd73c3aca43
jaime <jmchung@gabia.com>
2023-03-10 14:28:59 +0900

    feat: axios 토큰 만료일시 다시 accessToken을 얻은 후 재시도

--- src/api/common/interceptors.js
+++ src/api/common/interceptors.js
@@ -32,9 +32,6 @@ export function setInterceptors(instance) {
             originalRequest.headers.Authorization =
               'Bearer ' + store.state.member.token
             return instance(originalRequest)
-          } else {
-            store.commit('member/logout')
-            alert('권한이 없습니다. 다시 로그인 해주세요')
           }
         } catch (error2) {
           store.commit('member/logout')
